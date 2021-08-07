7094fe3f021f225682716aca89c088a80bd643f6
Andy Bond <andybond006@gmail.com>
2021-08-07 14:13:50 -0400

    feat: attempt to retrieve image color

--- src/_shortcodes/review.js
+++ src/_shortcodes/review.js
@@ -1,6 +1,6 @@
 module.exports = (item) => {
   return `<div class="review-item" data-type="${item.data.type}">
-  <img src="${item.data.page.url}${item.data.image}" loading="lazy">
+  <img src="${item.data.page.url}${item.data.image}" loading="lazy" style="--cover-color:${item.data.color}">
   <div class="info">
     <h1>${item.data.title}</h1>
     ${item.data.subtitle ? `<p class="subtitle">${item.data.subtitle}</p>` : ''}
